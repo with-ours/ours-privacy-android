d073c3f9c8c827b199bafd5d7c9effe94106b1c0
Steve Krenek <steve.krenek@gmail.com>
2025-06-05 17:03:59 -0500

    Moved to SDK 35.  Made sure it builds in Android Studio Meerkat.  Fixed a Mixpanel reference or two.

--- oursprivacydemo/build.gradle.kts
+++ oursprivacydemo/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.oursprivacy.oursprivacydemo"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.oursprivacy.oursprivacydemo"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
